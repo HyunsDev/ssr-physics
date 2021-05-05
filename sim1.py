9f1198e339694e28090a8ef6dc92bed07dc09a28
Hyuns <phw3071@gmail.com>
2021-05-06 01:43:34 +0900

    save

--- sim1.py
+++ sim1.py
@@ -7,18 +7,19 @@ from sympy import Symbol, solve
 # 월드 클래스
 class VirtualWorld:
     def __init__(self, measuring_time:int=10, interval=100, debug=False):
-            '''
-            가상 세계를 설정합니다.
+        '''
+        가상 세계를 설정합니다.
 
-            Args:
-                measuring_time : 측정 시간
-                interval : 시간 정확도 (1/interval초 마다 측정합니다.)
-                debug : 디버그 정보 출력 여부
-            '''
+        Args:
+            measuring_time : 측정 시간
+            interval : 시간 정확도 (1/interval초 마다 측정합니다.)
+            debug : 디버그 정보 출력 여부
+        '''
 
         self.measuring_time = measuring_time
         self.interval = interval
         self.debug = debug
+        self.start_time = time.time()
 
         self.objs = {}
         self.events = []
@@ -84,31 +85,68 @@ class VirtualWorld:
     def start(self):
         '''
         실험을 시작합니다. 생성된 물체와 추가된 이벤트를 진행시킵니다.
-        실험이 끝나면 
+        실험이 끝나면 실험 결과를 그래프로 띄웁니다.
         '''
-        result = {}
-        test_res_pos = []
+        obj_result = {}
         objs = self.objs
         events = self.events
 
-        for i in range(0, self.measuring_time*self.interval+1):
+        graphTypes = ['pos', 'v', 'Ek', 'p']
+        time_result = {}
+        for graphType in graphTypes:
+            time_result[graphType] = {}
+            for objName in objs:
+                time_result[graphType][objName] = []
+
+        if self.debug:
+            print(f"작업 시작 : {self.start_time}")
+                
+
+        # 메인 루프
+        for i in range(0, (self.measuring_time*self.interval)+1):
             # 디버그용 로그
             if self.debug:
                 # os.system('cls')
-                for objName in objs:
+                for objName in objs.keys():
                     now_time = f'{i // self.interval}초 {i % self.interval}/{self.interval}'
-                    print(f'{now_time} [초] ', end="")
-                    print('pos:{0:.5f}m, v:{0:.5f}m/s, Ek:{0:.5f}, p:{0:.5f}N'.format(objs[objName]['pos'],objs[objName]['v'],objs[objName]['Ek'],objs[objName]['p']))
+                    print(f'[{i}/{self.measuring_time*self.interval+1}]', end=" ")
+                    print(f'{now_time} 초', end=" ")
+                    print(f"pos:{objs[objName]['pos']}m, v:{objs[objName]['v']}m/s, Ek:{objs[objName]['Ek']}, p:{objs[objName]['p']}N")
+            
+            #기록 
+            obj_result[i] = {}
 
-            test_res_pos.append(objs[objName]['pos'])
+            for objName in objs.keys():
+                # print(objs)
+                obj = objs[objName]
+                # print('{0:.5f}'.format(objs[objName]['v']))
+                # print(obj['v'])
+                try:
+                    obj_result[i][objName].append({
+                        'pos' : obj['pos'],
+                        'v' : obj['v'],
+                        'Ek' : obj['Ek'],
+                        'p' : obj['p']
+                    })
+                except KeyError:
+                    obj_result[i][objName]=[]
+                    obj_result[i][objName].append({
+                        'pos' : obj['pos'],
+                        'v' : obj['v'],
+                        'Ek' : obj['Ek'],
+                        'p' : obj['p']
+                    })
+
+                for graphType in graphTypes:
+                    time_result[graphType][objName].append(obj[graphType])
 
             # 이벤트 계산
             for event in events:
-                event_time = [event["time"][0] * self.interval, event["time"][1] * self.interval]
+                event_time = range(event["time"][0] * self.interval, event["time"][1] * self.interval)
                 if i in event_time:
                     obj = objs[event['objName']]
                     obj["p"] += event["F"] / self.interval
-                    obj["v"] = obj["p"] / obj["m"] * self.interval
+                    obj["v"] = obj["p"] / obj["m"]
                     obj["Ek"] = (1/2) * obj["m"] * (obj["v"] ** 2)
             
             # 물체 위치 계산
@@ -117,11 +155,59 @@ class VirtualWorld:
                 obj['before_pos'] = obj['pos']
                 obj['pos'] += obj['v'] / self.interval
 
-        plt.plot(numpy.arange(0, self.measuring_time + (1/self.interval), 1/self.interval), test_res_pos)
+        if self.debug:
+            print(f"소모 시간 : {time.time() - self.start_time}s")
+
+        # 그래프
+        arange = numpy.arange(0, self.measuring_time + (1/self.interval), 1/self.interval)
+        plt.subplots(constrained_layout=True)
+
+        # 위치 그래프
+        plt.subplot(2, 2, 1)
+        plt.xlabel('time')
+        plt.ylabel('position(m)')
+        plt.title('Position Graph')
+        for objName in objs:
+            plt.plot(arange, time_result["pos"][objName], label=objName)
+        plt.legend()
+
+        # 속도 그래프
+        plt.subplot(2, 2, 2)
+        plt.xlabel('time')
+        plt.ylabel('speed(m/s)')
+        plt.title('Speed Graph')
+        for objName in objs:
+            plt.plot(arange, time_result["v"][objName], label=objName)
+        plt.legend()
+
+        # 운동 에너지 그래프
+        plt.subplot(2, 2, 3)
+        plt.xlabel('time')
+        plt.ylabel('Kinetic energy(J)')
+        plt.title('Kinetic energy Graph')
+        for objName in objs:
+            plt.plot(arange, time_result["Ek"][objName], label=objName)
+        plt.legend()
+
+        # 운동량 그래프
+        plt.subplot(2, 2, 4)
+        plt.xlabel('time')
+        plt.ylabel('momentu(N·s)')
+        plt.title('momentum Graph')
+        for objName in objs:
+            plt.plot(arange, time_result["p"][objName], label=objName)
+        plt.legend()
+            
+
+        # plt.plot(, test_res_pos)
         plt.show()
 
+        
+
 if __name__ == '__main__':
-    world = VirtualWorld(10, 100, debug=True)
+    world = VirtualWorld(15, 60, debug=True)
     world.add_square('one', 0, 1)
-    world.event_force_object("one", 2, [1,1])
+    world.event_force_object("one", 5, [1,8])
+    world.add_square('two', 0, 2)
+    world.event_force_object("two", 20, [4,8])
     world.start()
